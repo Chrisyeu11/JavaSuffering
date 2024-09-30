e940e8b4deae7701454763a9f20115484bc7f7e6
Chris <122718192+Chrisyeu11@users.noreply.github.com>
2024-09-29 20:33:17 -0500

    Add files via upload

---
+++ JAVASUFFERING/src/ch01/_02_EclipseSequence.java
@@ -0,0 +1,10 @@
+package ch01;
+
+public class _02_EclipseSequence {
+	public static void main(String[] args) {
+		
+		System.out.println("Write\nOnce");
+		System.out.println("사과	배	귤");
+		System.out.println("사과\t배\t귤");
+	}
+}
